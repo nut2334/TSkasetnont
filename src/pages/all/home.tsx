c5a0554e745dd6218fa4ea1f08366e004da4ee04
nut2334 <nut2334@gmail.com>
2024-02-15 22:02:36 +0700

    map

--- src/pages/all/home.tsx
+++ src/pages/all/home.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import { Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 const Home = (prop: { jwt_token: string }) => {
-    return (
-        <div>
-            {prop.jwt_token}
-        </div>
-    )
-}
+  return (
+    <div>
+      <MapContainer
+        center={[13.736717, 100.523186]}
+        zoom={13}
+        scrollWheelZoom={true}
+        style={{ height: "100vh", width: "100%" }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={[13.736717, 100.523186]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+      {prop.jwt_token}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
