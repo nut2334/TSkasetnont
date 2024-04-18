d0eef817ca9c95e84f91d9b4f7f8129a49389520
nut2334 <nut2334@gmail.com>
2024-04-19 03:27:51 +0700

    rdrgr

--- src/components/tab-products.tsx
+++ src/components/tab-products.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box, Divider, Tab, Typography, Container } from "@mui/material";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
@@ -10,13 +10,46 @@ import Myproducts from "../pages/farmer/myproducts";
 import Orderhistory from "../pages/farmer/orderhistory";
 import Analyze from "../pages/farmer/analyze";
 import Orderreserve from "../pages/farmer/orderreserve";
+import axios from "axios";
+import * as config from "../config/config";
+import { DataGrid } from "@mui/x-data-grid";
+
+interface memberInterface {
+  id: string;
+  customer_id: string;
+  purchase_count: number;
+  product_id: string;
+  firstname: string;
+  lastname: string;
+  phone: string;
+  username: string;
+}
 
 const TabProducts = (prop: { jwt_token: string; username: string }) => {
   const [value, setValue] = React.useState("1");
-
+  const [historyMember, setHistoryMember] = React.useState<memberInterface[]>(
+    []
+  );
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
+
+  useEffect(() => {
+    axios
+      .get(config.getApiEndpoint("membership", "GET"), {
+        headers: {
+          Authorization: `Bearer ${prop.jwt_token}`,
+        },
+      })
+      .then((response) => {
+        console.log(response.data.data);
+        setHistoryMember(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <Container maxWidth="lg">
       <TabContext value={value}>
@@ -47,6 +80,27 @@ const TabProducts = (prop: { jwt_token: string; username: string }) => {
           <Analyze jwt_token={prop.jwt_token} />
         </TabPanel>
         <TabPanel value="3">
+          <Typography variant="h6">ประวัติสมาชิก</Typography>
+          <DataGrid
+            rows={historyMember}
+            columns={[
+              { field: "username", headerName: "username", flex: 1 },
+              { field: "firstname", headerName: "ชื่อ", flex: 1 },
+              { field: "lastname", headerName: "นามสกุล", flex: 1 },
+              { field: "phone", headerName: "เบอร์", flex: 1 },
+              {
+                field: "purchase_count",
+                headerName: "จำนวนครั้งที่ซื้อ",
+                flex: 1,
+              },
+            ]}
+          />
+          <Divider
+            sx={{
+              marginTop: 2,
+              marginBottom: 2,
+            }}
+          />
           <Typography variant="h6">ประวัติการซื้อขาย</Typography>
           <Orderhistory jwt_token={prop.jwt_token} />
           <Divider />
--- src/pages/farmer/orderreserve.tsx
+++ src/pages/farmer/orderreserve.tsx
@@ -20,6 +20,7 @@ import InboxIcon from "@mui/icons-material/Inbox";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import Swal from "sweetalert2";
+
 interface OrderReserveInterface {
   id: string;
   reserve_products: productInterface;
